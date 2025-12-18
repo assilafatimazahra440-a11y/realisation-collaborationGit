00354c1bf6a8b9566ee96f2128c138a584bae8a9
fatima <assilafatimazahra440@gmail.com>
2025-12-18 11:07:31 +0000

    new branch

---
+++ guessNumber.js
@@ -0,0 +1,6 @@
+function guessNumber() {
+    let name = document.getElementById("name").value;
+    let guess = Number(document.getElementById("guess").value);
+    let message = document.getElementById("message");
+    let tries = document.getElementById("tries");
+}
