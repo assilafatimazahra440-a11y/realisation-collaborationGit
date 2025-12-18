02538afd3695500c27ad5c94661845d76ea82fa3
soukainaasaasslc25-boop <soukaina.asaas.slc25@gmail.com>
2025-12-18 11:51:06 +0000

    ajouter les condition

--- guessNumber.js
+++ guessNumber.js
@@ -3,4 +3,31 @@ function guessNumber() {
     let guess = Number(document.getElementById("guess").value);
     let message = document.getElementById("message");
     let tries = document.getElementById("tries");
+    if (!name) {
+    message.textContent = "Please enter your name!";
+    return;
+  }
+
+  counter++;
+
+  if (guess === secretNumber) {
+    message.textContent = counter === 1
+      ? "Bravo " + name + "! First try 👏"
+      : "Bravo " + name + "! You guessed it 🎉";
+    return;
+  }
+
+  if (counter === maxTries) {
+    message.textContent = "Sorry " + name + ", you reached the maximum tries 😢";
+    return;
+  }
+
+  if (guess < secretNumber) {
+    message.textContent = "Too low ⬇️";
+  } else {
+    message.textContent = "Too high ⬆️";
+  }
+  
+
+  tries.textContent = "Try: " + counter + " / " + maxTries;
 }
